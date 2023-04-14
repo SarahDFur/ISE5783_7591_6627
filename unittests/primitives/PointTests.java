6ce5e528680636362f004e7301c50465f1543d20
SarahDFur <Daatyah@gmail.com>
2023-04-14 17:03:40 +0300

    unittests - geometries getNormal tests

--- unittests/primitives/PointTests.java
+++ unittests/primitives/PointTests.java
@@ -1,22 +1,74 @@
 package primitives;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Unit tests for primitives.Point class
+ * @author Sarah Daatyah Furmanski and Efrat Kartman
+ */
 
 public class PointTests {
 
     @Test
     void add() {
+        // ============ Equivalence Partitions Tests ==============
+        //TC00: Addition with a positive result
+        Point p0 = new Point(0,0,0);
+        Vector v1 = new Vector(1,1,1);
+        assertEquals(new Point (1,1,1), p0.add(v1), "Incorrect addition");
+
+        // =============== Boundary Values Tests ==================
+        //TC01: Addition with a negative result
+        assertThrows(IllegalArgumentException.class,
+                ()-> p0.add(new Vector(0,0,0)),
+                "impossible addition");
     }
 
     @Test
     void subtract() {
+        Point p1 = new Point(2, 4, 3);
+        Point p2 = new Point(1,1,1);
+        Point p3 = new Point (-5,-6,-7);
+
+        // =============== Boundary Values Tests ==================
+        // TC00: Subtraction results in vector 0
+
+        assertThrows(IllegalArgumentException.class,
+                ()->p1.subtract(p1),
+                "Subtraction results in vector 0");
+
+        // ============ Equivalence Partitions Tests ==============
+        //TC01: Result is positive
+        Assertions.assertEquals(new Vector(1,3,2),p1.subtract(p2),"Vector subtraction with positive result failed");
+
+        //TC02: Result is negative
+        assertEquals(new Vector(-1,-3,-2),p2.subtract(p1),"Vector subtraction with negative result failed");
+
+        //TC03: Subtracting a negative number
+        assertEquals(new Vector(7,10,10),p1.subtract(p3),"Vector subtraction with negative vector failed");
     }
 
     @Test
     void distance() {
+        // ============ Equivalence Partitions Tests ==============
+        //TC00: Distance between two points is calculated correctly
+        assertEquals(
+                9,
+                new Point (0,0,0).distance(new Point (0,0,9)),
+                "Problem with distance calculation");
     }
 
     @Test
     void distanceSquared() {
+        // ============ Equivalence Partitions Tests ==============
+        //TC00: Squared distance between two points is calculated correctly
+        assertEquals(
+                3,
+                new Point (0,0,0).distanceSquared(new Point (1,1,1)),
+                "Problem with distance calculation");
     }
 }
--- unittests/primitives/VectorTests.java
+++ unittests/primitives/VectorTests.java
@@ -1,33 +1,156 @@
 package primitives;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static primitives.Util.isZero;
+
+/**
+ * Unit tests for primitives.Vector class
+ * @author Sarah Daatyah Furmanski and Efrat Kartman
+ */
+
 public class VectorTests {
 
+    /**
+     * Test method for {@link primitives.Vector#add(Vector)}.
+     * Checking BVA & EP between 2 vectors (additions)
+     */
+    @Test
+    public void add() {
+        Vector v1 = new Vector(1,1,1);
+        Vector v2 = new Vector (-1,-1,-1);
+        Vector v3 = new Vector (-5,-6,-7);
+        Vector v4 = new Vector(2, 4, 3);
 
-    @org.junit.jupiter.api.Test
-    void add() {
+        // =============== Boundary Values Tests ==================
+        //TC00: Addition results in vector 0
+        assertThrows(IllegalArgumentException.class,()->v1.add(v2), "Addition results in vector 0");
+        //TC01: Addition with 0
+        assertThrows(IllegalArgumentException.class,()->v1.add(new Vector(0,0,0)), "Addition with vector 0");
+
+        // ============ Equivalence Partitions Tests ==============
+        //TC02: Result is positive
+        assertEquals(new Vector(3,5,4), v4.add(v1), "Error in addition calculation");
+        //TC03: Result is negative
+        assertEquals(new Vector(-3,-2,-4), v4.add(v3), "Error in addition calculation");
     }
 
-    @org.junit.jupiter.api.Test
-    void scale() {
+    /**
+     * Test method for {@link primitives.Vector#scale(double)}.
+     * Checks scalar multiplication
+     */
+    @Test
+    public void scale() {
+        Vector v1 = new Vector(1,1,1);
+        Vector v2 = new Vector(-1,-1,-1);
+
+        // =============== Boundary Values Tests ==================
+        //TC00: Scaled with 0
+        assertThrows(IllegalArgumentException.class,
+                ()->v1.scale(0),
+                "Scaling with 0 failed");
+
+        // ============ Equivalence Partitions Tests ==============
+        //TC01: Scaled with positive number
+        assertEquals(new Vector(3,3,3),v1.scale(3),"Scaling with positive number failed");
+        //TC02: Scaled with negative number
+        assertEquals(new Vector(-3,-3,-3),v1.scale(-3),"Scaling with negative number failed");
+        //TC03: scale vector with negative values
+        assertEquals(new Vector(-3,-3,-3),v2.scale(3),"Scaling vector with negative values failed");
     }
 
-    @org.junit.jupiter.api.Test
-    void lengthSquared() {
+    /**
+     * Test method for {@link Vector#lengthSquared()}.
+     * Checks that the method for length (squared) works
+     */
+    @Test
+    public void lengthSquared() {
+        Vector v1 = new Vector(1, 2, 3);
+
+        // ============ Equivalence Partitions Tests ==============
+        //TC00: Length squared is calculated correctly
+        assertTrue(isZero(v1.lengthSquared() - 14),"ERROR: lengthSquared() wrong value");
     }
 
-    @org.junit.jupiter.api.Test
-    void length() {
+    /**
+     * Test method for {@link Vector#length()}.
+     *
+     */
+    @Test
+    public void length() {
+        // ============ Equivalence Partitions Tests ==============
+        //TC00: Length is calculated correctly
+        assertTrue(isZero(new Vector(0, 3, 4).length() - 5),"ERROR: length() wrong value");
     }
 
-    @org.junit.jupiter.api.Test
-    void dotProduct() {
+    /**
+     * Test method for {@link primitives.Vector#dotProduct(Vector)}.
+     *
+     */
+    @Test
+    public void dotProduct() {
+        Vector v1 = new Vector(1, 2, 3);
+        Vector v2 = new Vector(-2, -4, -6);
+        Vector v3 = new Vector(0, 3, -2);
+
+        // =============== Boundary Values Tests ==================
+        //TC00: dot product between orthogonal vectors
+        assertTrue(isZero(v1.dotProduct(v3)),"ERROR: dotProduct() for orthogonal vectors is not zero");
+        // ============ Equivalence Partitions Tests ==============
+        //TC01: checking that dot product is the correct value
+        assertTrue(isZero(v1.dotProduct(v2) + 28), "ERROR: dotProduct() wrong value");
     }
 
-    @org.junit.jupiter.api.Test
-    void crossProduct() {
+    /**
+     * Test method for {@link primitives.Vector#crossProduct(Vector)}.
+     *
+     */
+    @Test
+    public void crossProduct() {
+        Vector v1 = new Vector(1, 2, 3);
+        Vector v2 = new Vector(-2, -4, -6);
+        Vector v3 = new Vector(0, 3, -2);
+
+        // =============== Boundary Values Tests ==================
+        //TC00: two vectors are parallel
+        assertThrows(IllegalArgumentException.class,
+                ()->v1.crossProduct(v2),
+                "ERROR: crossProduct() for parallel vectors does not throw an exception" );
+
+        // ============ Equivalence Partitions Tests ==============
+        Vector vr = v1.crossProduct(v3);
+        //TC01.1: checking correct length for vr
+        assertTrue(isZero(vr.length() - v1.length() * v3.length()),"ERROR: crossProduct() wrong result length");
+        //TC01.2: checking vr is orthogonal to v1 and v3
+        assertTrue(isZero(vr.dotProduct(v1)) || !isZero(vr.dotProduct(v3)),
+                "ERROR: crossProduct() result is not orthogonal to its operands");
     }
 
-    @org.junit.jupiter.api.Test
-    void normalize() {
+    /**
+     * Test method for {@link Vector#normalize()}.
+     *
+     */
+    @Test
+    public void normalize() {
+        Vector v = new Vector(0, 0, 9);
+        Vector u = v.normalize();
+
+        // =============== Boundary Values Tests ==================
+        //TC00.1 checking that the length of the normalized vector is 1
+        assertTrue(isZero(u.length() - 1),"ERROR: the normalized vector is not a unit vector");
+
+        //TC00.2 checking that the normalized vector is parallel to the original vector
+        assertThrows(IllegalArgumentException.class,
+                ()->v.crossProduct(u),
+                "ERROR: the normalized vector is not parallel to the original one");
+
+        //TC00.3 checking that the normalized vector is in the same direction as the original vector
+        assertTrue(v.dotProduct(u) > 0, "ERROR: the normalized vector is opposite to the original one");
+
+        // ============ Equivalence Partitions Tests ==============
+        //TC01: checking that the vector was normalized correctly
+        assertEquals(new Vector (0,0,1),u, "Vector was normalized incorrectly");
     }
 }
