e592e7e424a74c1f8d724508087f5140467294ef
SarahDFur <Daatyah@gmail.com>
2023-04-23 12:59:52 +0300

    -> SphereTests & GeometriesTests reformatting

--- src/geometries/Geometries.java
+++ src/geometries/Geometries.java
@@ -8,13 +8,13 @@ import java.util.List;
 
 public class Geometries implements Intersectable {
 
-    private List<Intersectable> geometriesList;
+    private List<Intersectable> geometries;
 
     /**
      * default constructor
      */
     public Geometries() {
-        this.geometriesList = new LinkedList<>();
+        this.geometries = new LinkedList<>();
     }
 
     /**
@@ -33,13 +33,13 @@ public class Geometries implements Intersectable {
      */
     public void add(Intersectable... geometries) {
         if (geometries.length > 0)
-            this.geometriesList.addAll(List.of(geometries));
+            this.geometries.addAll(List.of(geometries));
     }
 
     @Override
     public List<Point> findIntersections(Ray ray) {
         List<Point> intersections = null;
-        for (Intersectable geo : geometriesList) {
+        for (Intersectable geo : geometries) {
             List<Point> temp = geo.findIntersections(ray);
             if (temp != null) {
                 if (intersections == null)
--- unittests/geometries/GeometriesTests.java
+++ unittests/geometries/GeometriesTests.java
@@ -16,36 +16,38 @@ public class GeometriesTests {
 
     @Test
     public void testFindIntersections() {
-        Geometries collection = new Geometries();
-        collection.add(new Plane(new Point(1, 0, 0), new Point(0, 1, 0), new Point(0, 0, 2)));
-        collection.add(new Sphere(1, new Point(1, 1, 1)));
-        collection.add(new Triangle(new Point(1, 0, 0), new Point(0, 1, 0), new Point(0, 0, 1)));
+        Geometries geometries = new Geometries();
+        geometries.add(
+                new Plane(new Point(1, 0, 0), new Point(0, 1, 0), new Point(0, 0, 2)),
+                new Sphere(1, new Point(1, 1, 1)),
+                new Triangle(new Point(1, 0, 0), new Point(0, 1, 0), new Point(0, 0, 1))
+        );
 
         // ============ Equivalence Partitions Tests ==============
         // EP1: Ray intersects with some Geometries, but not all
         Ray ray = new Ray(new Point(2, 2, 2.5), new Vector(-1, -1, -2));
-        assertEquals(3, collection.findIntersections(ray).size(),
+        assertEquals(3, geometries.findIntersections(ray).size(),
                 // intersect plane and sphere, but not triangle
                 "Wrong number of intersections points - some geometries intersected");
 
         // =============== Boundary Values Tests ==================
         // BVA1: Ray doesn't intersect with any of Geometries
         ray = new Ray(new Point(2, 2, 2.5), new Vector(0, 0, 1));
-        assertNull(collection.findIntersections(ray),
+        assertNull(geometries.findIntersections(ray),
                 "Wrong number of intersections points - no geometries intersected");
         // BVA2: Ray intersects with only one of Geometries
         ray = new Ray(new Point(2, 2, 2.5), new Vector(-1, 0, 0));
-        assertEquals(1, collection.findIntersections(ray).size(),
+        assertEquals(1, geometries.findIntersections(ray).size(),
                 // intersect only plane, and not triangle and sphere
                 "Wrong number of intersections points - only one geometries intersected");
         // BVA3: Ray intersects with all the Geometries
         ray = new Ray(new Point(2, 2, 2.5), new Vector(-3, -3, -2.5));
-        assertEquals(4, collection.findIntersections(ray).size(),
+        assertEquals(4, geometries.findIntersections(ray).size(),
                 "Wrong number of intersections points - all geometries intersected");
-        // BVA4: Empty Geometries collection
+        // BVA4: Empty Geometries geometries
         ray = new Ray(new Point(2, 2, 2.5), new Vector(1, 1, 1));
         assertNull(new Geometries().findIntersections(ray),
-                "Wrong number of intersections points - empty collection");
+                "Wrong number of intersections points - empty geometries");
     }
 
 }
