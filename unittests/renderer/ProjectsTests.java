45a5cac4c5e2608873fe89c46d628e1b2ad54645
SarahDFur <Daatyah@gmail.com>
2023-06-05 14:05:50 +0300

    picture - needs fixing

--- unittests/renderer/ProjectsTests.java
+++ unittests/renderer/ProjectsTests.java
@@ -38,8 +38,6 @@ public class ProjectsTests {
     public void myPicture() {
         Scene scene = new Scene("Test scene")
                 .setAmbientLight(new AmbientLight(new Color(WHITE), new Double3(0.15)));
-
-        //@TODO: i have no idea what is going on
         // sometimes thing works as they should but usually not,
         // i dont
         scene.geometries.add(
@@ -53,46 +51,52 @@ public class ProjectsTests {
                         //.setEmission(new Color(RED))
                         .setMaterial(new Material()
                                 .setKd(0.5).setKs(0.5).setShininess(20))
-                ,new Cylinder(60d,new Ray(new Point(0,-85,-40),new Vector(0,1,0)),90d)
-                        .setEmission(new Color(blue))
+                ,new Cylinder(60d,new Ray(new Point(0,-80,-40),new Vector(0,1,0)),90d)
+                     //   .setEmission(new Color(20,20,190))
                         .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60)
-                                .setKr(0.000001).setKt(0.4))
-                ,new Sphere(40d, new Point(0,-50,-40))
-                        .setEmission(new Color(0, 50, 100))
+                                .setKr(0.4).setKt(0.99))
+                ,new Cylinder(55d,new Ray(new Point(0,-78,-40),new Vector(0,1,0)),60d)
+                        .setEmission(new Color(orange))
+                        .setMaterial(new Material()
+                                .setKr(0.3).setKt(0.99))
+                ,new Sphere(40d, new Point(0,-40,-40))
+                        .setEmission(new Color(20, 0, 80))
                         .setMaterial(
-                                new Material().setKd(0.25).setKs(0.25).setShininess(20)
-                                        .setKt(0.00000025).setKr(0.0001)
+                                new Material().setKd(0.25).setKs(0.25).setShininess(61)
+                                        .setKt(0.99).setKr(0.4)
                         )
 //                ,new Sphere(10d,new Point(20,20,-20))
-                //                       .setEmission(new Color(GREEN))
+//                        .setEmission(new Color(GREEN))
+//                        .setMaterial(new Material().setKd(0.25).setKs(0.25).setKr(0.2).setKt(0.999))
 
-//                ,new Polygon(new Point(-150, -150, -135), new Point(150, -150, -135))
+//                ,new Polygon(new Point(-150, -150, -135), new Point(150, -150, -135),
 //                        new Point(75, 75, -115), new Point(-75, 75, -115))
 //                        .setEmission(new Color(RED))
 //                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60))
 //               ,new Sphere(40, new Point(30,30,-50))
-//                        .setEmission(new Color(20, 20, 20))
-//                        .setMaterial(new Material().setKr(new Double3(0.5, 0, 0.4))) //reflective
-//                ,new Sphere(50,new Point(70,-70,-70))
+//                        .setEmission(new Color(120, 20, 200))
+//                        .setMaterial(new Material().setKr(new Double3(0.5, 0, 0.4)).setKt(0.62)) //reflective
+//                ,new Sphere(30,new Point(90,-50,-80))
 //                        .setEmission(new Color(RED))
 //                        .setMaterial(new Material().setKd(0.25).setKs(0.25).setShininess(20)
 //                                .setKt(new Double3(0.5, 0, 0))) //transparent
-//                , new Sphere(30d, new Point(30, 50, -50))
-//                        .setEmission(new Color(BLUE))
-//                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)) //transparent
-//                ,new Sphere(30d, new Point(60, -50, -50))
-//                        .setEmission(new Color(BLUE))
-//                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(30))
+
                 ,new Tube(10,new Ray(new Point(-80,-80,-80),new Vector(0,1,0)))
                         .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKr(0.0002).setKt(0.45))
         );
 
-        scene.lights.add(new PointLight(new Color(ORANGE), new Point(30, 70, -100))
-                .setKl(0.001).setKq(0.0000002));
+        scene.lights.add(
+                new PointLight(new Color(ORANGE), new Point(30, 70, -100))
+                        .setKl(0.001).setKq(0.0000002)
+        );
+//        scene.lights.add(new SpotLight(new Color(ORANGE), new Point(30, 70, -100), new Vector(0,1,0))
+//                .setKl(0.001).setKq(0.0000002));
 //        scene.lights.add(new SpotLight(new Color(700, 400, 400),
 //                new Point(30, 50, 0), new Vector(0, 0, -1)).setKl(4E-5).setKq(2E-7));
-//        scene.lights.add(new SpotLight(new Color(400, 400, 700),
+//        scene.lights.add(new SpotLight(new Color(orange),
 //                new Point(100, -10, 115), new Vector(-1, -1, -4)).setNarrowBeam(10).setKl(4E-4).setKq(2E-5));
+        scene.lights.add(new SpotLight(new Color(orange),
+                new Point(0,-78,-40),new Vector(0,1,0)).setNarrowBeam(10).setKl(4E-4).setKq(2E-5));
 
 //        Camera camera = new Camera(new Point(0, 0, 1500), new Vector(0, 0, -1), new Vector(0, 1, 0))
 //                .setViewPlaneSize(200, 200).setViewPlaneDistance(1000)
@@ -101,11 +105,16 @@ public class ProjectsTests {
 //                .renderImage();
 //        camera.writeToImage();
 
-        Camera camera = new Camera(new Point(0, 0, 1500), new Vector(0, 0, -1), new Vector(0, 1, 0))
+        Camera camera = new Camera(new Point(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0))
                 .setViewPlaneSize(200, 200).setViewPlaneDistance(1000)
                 .setRayTracer(new RayTracerBasic(scene))
                 .setImageWriter(new ImageWriter("myPicture-7.3", 700, 700))
                 .renderImage();
+//        Camera camera = new Camera(new Point(0, 100, 1000), new Vector(0, -1, 0), new Vector(1, 0, -1))
+//                .setViewPlaneSize(200, 200).setViewPlaneDistance(1000)
+//                .setRayTracer(new RayTracerBasic(scene))
+//                .setImageWriter(new ImageWriter("myPicture-7.3", 700, 700))
+//                .renderImage();
         camera.writeToImage();
     }
 
